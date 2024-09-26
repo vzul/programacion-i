2d6095d5cc365afc81f59200e18c300124d26c9b
vzul <71192680+vzul@users.noreply.github.com>
2024-09-26 09:17:19 +0200

    Update sumOfNumbers.java

--- Recursion/sumOfNumbers.java
+++ Recursion/sumOfNumbers.java
@@ -1,1 +1,23 @@
+/******************************************************************************
 
+Calculate the sum of all numbers from 1 to n
+
+*******************************************************************************/
+public class Main
+{
+    static int sumOf(int n){
+        
+        if(n == 0){
+            return 0;
+        } else {
+            return n + sumOf(n-1);
+        }
+    }
+    
+	public static void main(String[] args) {
+	    
+	    int x = 7;
+	    System.out.println("Sum of numbers: " + sumOf(x));
+	   
+	}
+}
