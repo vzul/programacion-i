811eb2f1f8097f87df9a1fab6e1ebaf3988e5a4d
vzul <71192680+vzul@users.noreply.github.com>
2024-09-26 09:09:25 +0200

    Create factorial.java

---
+++ factorial.java
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+Factorial
+
+*******************************************************************************/
+public class Main
+{
+    static int factorial(int n){
+        if(n == 0 || n == 1){
+            return 1;
+        } else {
+            return n * factorial (n-1);
+        }
+    }
+    
+	public static void main(String[] args) {
+	    
+	    int x = 4;
+	    System.out.println("Factorial: " + factorial(x));
+	   
+		
+	}
+}
