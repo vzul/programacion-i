3413738262d58f7afee62165440913bae0b1d7a9
vzul <71192680+vzul@users.noreply.github.com>
2024-10-01 13:14:39 +0200

    Update getCharAtIndexOfString.java

--- getCharAtIndexOfString.java
+++ getCharAtIndexOfString.java
@@ -6,6 +6,7 @@ Get the character at the given index within the string.
 *******************************************************************************/
 public class Main
 {
+	// METHOD 1: CONVERTING STRING TO CHAR ARRAY
     static int getChar(char target, char [] strAux){
       
         for (int i = 0; i < strAux.length; i++){
@@ -14,6 +15,20 @@ public class Main
              }
         } return -1;
     } 
+
+	// METHOD 2: USING .CHARAT METHOD
+
+static char getChar2(String strAux, int position){
+      return strAux.charAt(position);
+    } 
+
+	// METHOD 3: RETURNING ASCII NUMBER 
+
+   static int getChar3(String strAux, int position){
+      int index1 = strAux.charAt(position);
+      return index1;
+    } 
+     
      
     
 	public static void main(String[] args) {
@@ -21,5 +36,8 @@ public class Main
 		String str =  new String("Hello world");
 		char[] charArray = str.toCharArray();
 		System.out.println("Index of e: " + getChar(letter, charArray));
+
+		System.out.println("Char at position 0: " + getChar2(str, 0));
+		System.out.println("Char at position 10: " + getChar2(str, 10))
 	}
 }
