18affc3b2eaab6d47fa57a3e9ac1f0a4a8d7b8ec
vzul <71192680+vzul@users.noreply.github.com>
2024-10-03 09:54:50 +0200

    Update addFirst100Numbers.java

--- addFirst100Numbers.java
+++ addFirst100Numbers.java
@@ -1,1 +1,28 @@
+/******************************************************************************
 
+Print the sum of the first 100 numbers.
+
+*******************************************************************************/
+public class Main
+{
+    static int sum (int num){
+        
+        
+        if (num == 1) {
+            return 1;
+        } else {
+            return num + sum(num - 1);
+            
+        }
+       
+            
+    
+    }
+	
+	public static void main(String[] args) {
+	   
+	   
+	   System.out.println("Sum of first 100 numbers: " + sum(100));
+	   
+	}
+}
