e9983480500bed70b17e246cc4e1e25bcba905ca
vzul <71192680+vzul@users.noreply.github.com>
2024-10-28 17:44:33 +0100

    Update reverseIntegerWhile.java

--- Exams/reverseIntegerWhile.java
+++ Exams/reverseIntegerWhile.java
@@ -1,10 +1,8 @@
 static int reverseInteger(int n){
-        int first = 0;
         int last = 0;
         int reversed = 0;
     
        while(n > 0){
-            first = n / 10;
             last = n % 10;
             reversed = reversed * 10 + last;
             n = n / 10;
