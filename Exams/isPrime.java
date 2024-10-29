bb09926c449bebfeb3f01059dc469d379bf1d29a
vzul <71192680+vzul@users.noreply.github.com>
2024-10-29 13:28:50 +0100

    Create isPrime.java

---
+++ Exams/isPrime.java
@@ -0,0 +1,13 @@
+static boolean isPrime(int n){
+        
+        // base case:
+        if(n < 2){
+            return false;
+        }
+            
+        for(int i = 2; i < Math.sqrt(n); i++){
+            if(n % i == 0){
+                return false;
+            }
+        } return true;
+}
