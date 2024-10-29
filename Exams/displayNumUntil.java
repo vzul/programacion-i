dcaba2883ac7edacd1e05110643f61eaa7d95dc5
vzul <71192680+vzul@users.noreply.github.com>
2024-10-29 14:10:34 +0100

    Create displayNumUntil.java

---
+++ Exams/displayNumUntil.java
@@ -0,0 +1,11 @@
+static void displayUntil(int n){
+        for(int i=0; i <= n; i++){
+            System.out.println(i);
+        }
+    }
+
+    
+	public static void main(String[] args) {
+		System.out.println("Display up to 24 ");
+		displayUntil(24);
+    }
