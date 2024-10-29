d2b41bd78d4a63d88e9eb2b41f3a7f7564d49893
vzul <71192680+vzul@users.noreply.github.com>
2024-10-29 11:54:57 +0100

    Update largestDigit.java

--- Exams/largestDigit.java
+++ Exams/largestDigit.java
@@ -1,1 +1,11 @@
-
+static int largDig(int n){
+        
+        int largest = 0;
+      
+       for(; n > 0; n /= 10){
+           int last = n % 10; 
+               if (last > largest){
+               largest = last;
+                }
+       } return largest;
+    }
