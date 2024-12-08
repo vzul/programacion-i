f2491500995c1eed0f5e460e0154bce63109e21f
vzul <71192680+vzul@users.noreply.github.com>
2024-12-08 12:18:44 +0100

    Update minValue.java

--- Arrays/Recursion/minValue.java
+++ Arrays/Recursion/minValue.java
@@ -1,1 +1,25 @@
+    // Wrapper method to start recursion
+    static int minValue(int[] col) {
+        if (col.length == 0) {
+            throw new IllegalArgumentException("Array cannot be empty");
+        }
+        return minValueAux(col, 0);
+    }
 
+    // Recursive method to find the minimum value
+    static int minValueAux(int[] col, int i) {
+        // Base case: If this is the last element, return it
+        if (i == col.length - 1) {
+            return col[i];
+        }
+
+        // Recursive step: Find the minimum value in the rest of the array
+        int restMin = minValueAux(col, i + 1);
+
+        // Compare current element with the minimum of the rest
+        if (col[i] < restMin) {
+            return col[i];
+        } else {
+            return restMin;
+        }
+    }
