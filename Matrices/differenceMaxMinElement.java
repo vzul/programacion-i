fe7ba35071967f45584841b6684dbf5ec04bf2ac
vzul <71192680+vzul@users.noreply.github.com>
2024-12-16 10:04:55 +0100

    Create differenceMaxMinElement.java

---
+++ Matrices/differenceMaxMinElement.java
@@ -0,0 +1,21 @@
+static void check(int[][] m){
+        
+        int max = m[0][0];
+        int min = m[0][0];
+        
+        for(int i = 0; i < m.length; i++){
+            for(int j = 0; j < m[i].length; j++){
+                if(m[i][j] < min){
+                    min = m[i][j];
+                } else if (m[i][j] > max){
+                    max = m[i][j];
+                }
+            }
+        } 
+        
+        System.out.println("Max: " + max);
+        System.out.println("Min: " + min);
+        
+        int difference = max - min;
+        System.out.println("Difference between largest and smallest element: " + difference);
+    }
