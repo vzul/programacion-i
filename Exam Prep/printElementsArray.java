918e5b3f17797d05a07876e477e415fd8d61382a
vzul <71192680+vzul@users.noreply.github.com>
2024-12-24 13:14:28 +0100

    Create printElementsArray.java

---
+++ Exam Prep/printElementsArray.java
@@ -0,0 +1,5 @@
+void printArray(int array[]) {
+        for(int i = 0; i < array.length; i++){
+            System.out.print(array[i] + " "); // PRINT INSTEAD OF PRINTLN to not print on new line
+        }
+    }
