30d7db4331317918d2dba586351cfb35b7727ca2
vzul <71192680+vzul@users.noreply.github.com>
2024-12-27 16:49:53 +0100

    Create sumOfAlternateProduct.java

---
+++ Exam Prep/Interesting/sumOfAlternateProduct.java
@@ -0,0 +1,9 @@
+static int findSum(int arr[]) {
+       int sum = 0;
+       
+       for(int i = 0; i < arr.length; i++){
+           if(i % 2 == 0){
+               sum += arr[i] * i;
+           }
+       } return sum;
+   }
